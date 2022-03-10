e569adcf2fd9c5bb249ae61bc96467c4390e9146
Vladislav Tkachenko <webkadiz@gmail.com>
2022-03-10 22:19:50 +0300

    add *_file functions to utils

--- utils.asm
+++ utils.asm
@@ -68,6 +68,63 @@ setCursorPosition:
     pop bp
     ret
 
+
+; [bp+4] first param - address of file name string
+open_file:
+    push bp
+    mov bp, sp
+    mov ah, 3dh
+    mov al, 2               ; read/write mode
+    mov dx, [bp+4]
+    int 21h
+    pop bp
+    ret 2
+
+
+; [bp+4] first param  - file handle number
+; [bp+6] second param - address of data to write
+; [bp+8] third param  - count bytes for write
+write_file:
+    push bp
+    mov bp, sp
+    mov ah, 40h
+    mov bx, [bp+4]
+    mov cx, [bp+8]
+    mov dx, [bp+6]
+    int 21h
+    pop bp
+    ret 6
+
+
+; [bp+4] first param - file handle number
+close_file:
+    push bp
+    mov bp, sp
+    mov ah, 3eh
+    mov bx, [bp+4]
+    int 21h
+    pop bp
+    ret 2
+
+
+; [bp+4] first param  - address of file name string
+; [bp+6] second param - address of data to write
+; [bp+8] third param  - count bytes for write
+owc_file:
+    push bp
+    mov bp, sp
+    push word [bp+4]
+    call open_file
+    push ax
+    push word [bp+8]
+    push word [bp+6]
+    push ax
+    call write_file
+    call close_file
+    pop bp
+    ret 6
+
+
 %endif
 
 skipUtils:
